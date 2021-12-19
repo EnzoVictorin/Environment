8602e8aeefb72add1e6013d2d29016bdb38b0c31
Enzo VICTORIN <enzovictorin@protonmail.com>
2021-12-19 18:48:32 +0100

    Add documentation.

---
+++ README.md
@@ -0,0 +1,43 @@
+# Enzo VICTORIN's environment installer
+
+Here is the script I use to configure a new Apple computer with all my development tools.
+
+# What does it do?
+It installs and configures:
+ - Apple's Command Line Tools
+ - [Homebrew](https://brew.sh/)
+ - [Oh My ZSH](https://ohmyz.sh/)
+    - Theme: [PowerLevel10k](https://github.com/romkatv/powerlevel10k)
+    - Plugins: [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions) and [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting.git)
+ - [Cocoapods](https://cocoapods.org/)
+ - [Xcodes](https://github.com/RobotsAndPencils/XcodesApp)
+ - [VSCodium](https://vscodium.com/)
+ - [LibreWolf](https://librewolf.net/)
+    - With:
+        - [uBlock Origin](https://github.com/gorhill/uBlock#ublock-origin)
+        - [Medium Unlimited](https://addons.mozilla.org/en-US/firefox/addon/medium-unlimited-read-for-free/)
+        - [Firefox multi-account container](https://addons.mozilla.org/en-US/firefox/addon/multi-account-containers/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=homepage-primary-hero)
+        - [Dashlane](https://www.dashlane.com/)
+        - [SponsorBlock](https://sponsor.ajay.app/)
+        - [Forget Me Not](https://addons.mozilla.org/en-US/firefox/addon/forget_me_not/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search)
+
+The scripts to install `Homebrew` and `Oh My ZSH` are located in [./installers](./installers). They were downloaded from their respective websites.
+The browser extensions are located in [./LibreWolfExtensions](./LibreWolfExtensions). Remember to update them!
+
+The script will always ask your permission before doing anything.
+
+# Usage
+
+```bash
+$ chmod +x ./install.sh
+$ ./install.sh
+```
+
+## Browser extensions
+After running this script, LibreWolf will not load the extensions. Go to the settings menu of LibreWolf to fix it.
+
+# License
+This script is licensed under [GNU GPLv3](./LICENSE)
+This script comes with **ABSOLUTELY NO WARRANTY**.
+This is free software, and you are welcome to redistribute it
+under certain conditions; see ./LICENSE.
--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Enzo's environment installer
+# License: GNU GPLv3
+# See ./LICENSE for more information.
 
+
+# COLORS
 RED='\033[0;31m'
 WHITE='\033[0;37m'
 GREEN="\033[0;32m"
@@ -10,36 +15,49 @@ RED_ARROW="$RED==>"
 
 cd "$( dirname "$0" )"
 
+# MARK: Functions
 
+# Check if this script is executed as root.
+# Don't run it as root!
 checkSudo() {
     if [ $(whoami) = 'root' ]; then
         echo -e "$RED_ARROW \033[1mDon't run this script as root!\033[0m"
         exit 1
     fi
-}
+
+# Install Apple's Command line tools
 installCommandLineTools() {
     echo -e "$ARROW \033[1mInstalling Command Line Tools...\033[0m"
     xcode-select --install
 }
+
+# Install Homebrew: The missing package manager for macOS from the install script in ./installers
+# They were downloaded from the official website.
+# It is required for installing everything else.
 installBrew() {
     if command -v brew &> /dev/null
     then
        echo -e "$ARROW \033[1mHomebrew is already installed!\033[0m"
     else
         echo -e "$ARROW \033[1mInstalling HomeBrew\033[0m"
-        chmod +x helpers/homebrew_install.sh
-        /bin/bash -c helpers/homebrew_install.sh
+        chmod +x installers/homebrew_install.sh
+        /bin/bash -c installers/homebrew_install.sh
     fi
 }
+
+# Install Oh my ZSH to manage your terminal configuration from the install script in ./installers.
+# They were downloaded from the offical website.
 installOhMyZSH() {
     echo -e "$ARROW \033[1mInstalling \`Oh My Zsh\`...\033[0m"
-    chmod +x helpers/ohMyZSH_install.sh
-    sh helpers/ohMyZSH_install.sh --unattended
-    echo -e "$ARROW \033[1mInstalling \`Oh My Zsh\` zshrc...\033[0m"
+    chmod +x installers/ohMyZSH_install.sh
+    sh installers/ohMyZSH_install.sh --unattended
+    echo -e "$ARROW \033[1mInstalling \`Oh My Zsh\` configuration...\033[0m"
     cp dotFiles/zshrc ~/.zshrc
     chmod +x ~/.zshrc
     chmod +x ~/.oh-my-zsh
 }
+
+# Install the ZSH theme `PowerLevel10K`
 installZSH_Theme() {
     echo -e "$ARROW \033[1mInstalling ZSH theme \`Powerlevel10K\`...\033[0m"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -47,45 +65,56 @@ installZSH_Theme() {
     cp dotFiles/p10K.zsh ~/.p10k.zsh
 }
 
+# Install two ZSH plugins
+# zsh-autocomplete and zsh-syntax-highlightning.
 installZSH_Plugins() {
-    echo -e "$ARROW \033[1mInstalling ZSH plugin \`zsh-autocomplete\`...\033[0m"
+    echo -e "$ARROW \033[1mInstalling ZSH plugin \`zsh-autosuggestions\`...\033[0m"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     echo -e "$ARROW \033[1mInstalling ZSH plugin \`zsh-syntax-highlighting\`...\033[0m"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
 
+# Install Cocoapods
 installCocoapods() {
     echo -e "$ARROW \033[1mInstalling cocoapods...\033[0m"
     echo -e "Your password is required for installing Cocoapods."
     sudo gem install cocoapods
 }
 
+# Install Fastlane
 installFastlane() {
     echo -e "$ARROW \033[1mInstalling Fastlane...\033[0m"
     brew install fastlane
 }
 
+# Install Xcodes
+#
 installXcodes() {
     echo -e "$ARROW \033[1mInstalling Xcodes...\033[0m"
     brew install --cask xcodes
 }
 
+# Install VSCodium: a fork of VSCode without telemetry.
 installVSCodium() {
     echo -e "$ARROW \033[1mInstalling VSCodium...\033[0m"
     brew install --cask vscodium
 }
 
+# Install LibreWolf: a fork of Firefox without telemetry.
 installLibreWolf() {
     echo -e "$ARROW \033[1mInstalling LibreWolf...\033[0m"
     brew install --cask librewolf
 }
 
+# Install a few usefull firefox extensiosn
+# See ./LibreWolfExtensions/README.md for details.
 installLibreWolfExtensions() {
     echo -e "$ARROW \033[1mInstalling LibreWolf extensions...\033[0m"
     echo -e "see ./LibreWolfExtensions/README.md for details."
     cp LibreWolfExtensions/** $HOME/Library/Application\ Support/LibreWolf/Profiles/obgtuti3.default-release/extensions
 }
 
+# Install everything!
 install() {
     # Install Apple's Command line tools.
     installCommandLineTools
@@ -117,8 +146,12 @@ install() {
     exit 0
 }
 
+# MARK: SCRIPT
+
+# You should never run this script as root (e.g sudo)
 checkSudo
 
+# This script will ask your authorisation before installing anything!
 echo -e "\n$BLUE\033[1mWelcome to Enzo VICTORIN's environment installer!\033[0m"
 echo -e "
 Copyright (C) 2021 Enzo VICTORIN
@@ -131,7 +164,7 @@ echo -e "$ARROW \033[1mApple's Command Line Tools\033[0m"
 echo -e "$ARROW \033[1mHomebrew\033[0m"
 echo -e "$ARROW \033[1mOh My ZSH\033[0m"
 echo -e "$ARROW \033[1mOh My ZSH theme \`Powerlevel10K\`\033[0m"
-echo -e "$ARROW \033[1mOh My ZSH plugin \`zsh-autocomplete\`\033[0m"
+echo -e "$ARROW \033[1mOh My ZSH plugin \`zsh-autosuggestions\`\033[0m"
 echo -e "$ARROW \033[1mOh My ZSH plugin \`zsh-syntax-highlighting\`\033[0m"
 echo -e "$ARROW \033[1mCocoapods\033[0m"
 echo -e "$ARROW \033[1mXcodes\033[0m"
@@ -154,7 +187,7 @@ echo -e "$ARROW \033[1mSponsorBlock\033[0m"
 echo -e "$ARROW \033[1mAuto Cookie Optout\033[0m"
 echo -e "$ARROW \033[1mForget Me Not\033[0m"
 
-
+# yes, it really does it!
 read -p $'Do you wish to continue? [y/n] \x0a' yn
 case $yn in
     [Yy]* ) install;;
